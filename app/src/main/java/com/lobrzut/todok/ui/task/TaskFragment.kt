8c2e91a2d152f9842171f23cc88a1b355732da08
lobrzut <48508079+lobrzut@users.noreply.github.com>
2021-07-22 17:45:34 +0200

    errorTaskFragment

--- app/src/main/java/com/lobrzut/todok/ui/task/TaskFragment.kt
+++ app/src/main/java/com/lobrzut/todok/ui/task/TaskFragment.kt
@@ -1,13 +1,16 @@
 package com.lobrzut.todok.ui.task
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.lobrzut.todok.R
+import com.lobrzut.todok.database.TaskEntry
 import com.lobrzut.todok.databinding.FragmentTaskBinding
 import com.lobrzut.todok.viewmodel.TaskViewModel
 
@@ -27,18 +30,77 @@ class TaskFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        adapter = TaskAdapter(TaskClickListener { taskEntry -> findNavController().navigate(TaskFragmentDirections.actionTaskFragmentToUpdateFragment(taskEntry)) })
-        viewModel.getAllTasks.observe(viewLifecycleOwner){
+        adapter = TaskAdapter(TaskClickListener { taskEntry ->
+            findNavController().navigate(
+                TaskFragmentDirections.actionTaskFragmentToUpdateFragment(
+                    taskEntry
+                )
+            )
+        })
+        viewModel.getAllTasks.observe(viewLifecycleOwner) {
             adapter.submitList(it)
         }
 
 
         binding.apply {
             binding.recyclerView.adapter = adapter
-            floatingActionButton.setOnClickListener{findNavController().navigate(R.id.action_taskFragment_to_addFragment)
+            floatingActionButton.setOnClickListener {
+                findNavController().navigate(R.id.action_taskFragment_to_addFragment)
+            }
         }
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val taskEntry = adapter.currentList[position]
+                viewModel.delete(taskEntry)
+
+                Snackbar.make(binding.root, "Deleted!", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        viewModel.insert(taskEntry)
+                    }
+                    show()
+                }
+            }
+
+        }).attachToRecyclerView(binding.recyclerView)
+        setHasOptionsMenu(true)
 
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.task_menu, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_delete_all -> deleteAllItem()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAllItem() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Delete All")
+            .setMessage("Are You sure?")
+            .setPositiveButton("Yes") { dialog, _ ->
+                viewModel.deleteAll()
+                dialog.dismiss()
+            }.setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }.create().show()
+    }
+
 }
