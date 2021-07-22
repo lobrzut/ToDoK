38565ff9b42c9643b12cd0cebb1a7b20335156be
lobrzut <48508079+lobrzut@users.noreply.github.com>
2021-07-22 10:55:42 +0200

    LayoutRepair/appWorks

--- app/src/main/java/com/lobrzut/todok/database/TaskDao.kt
+++ app/src/main/java/com/lobrzut/todok/database/TaskDao.kt
@@ -4,22 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
-abstract class TaskDao {
-
-    //abstract!!
+interface TaskDao {
 
     @Insert
-    abstract suspend fun insert(taskEntry: TaskEntry)
+    fun insert(taskEntry: TaskEntry)
 
     @Delete
-    abstract suspend fun delete(taskEntry: TaskEntry)
+    fun delete(taskEntry: TaskEntry)
 
     @Update
-    abstract suspend fun update(taskEntry: TaskEntry)
+    fun update(taskEntry: TaskEntry)
 
     @Query("DELETE FROM task_table")
-    abstract suspend fun deleteAll()
+    fun deleteAll()
 
     @Query("SELECT * FROM task_table ORDER BY timestamp DESC")
-    abstract fun getAllTasks():LiveData<List<TaskEntry>>
+    fun getAllTasks():LiveData<List<TaskEntry>>
 }
