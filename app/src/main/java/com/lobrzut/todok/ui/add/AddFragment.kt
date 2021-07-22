bf8840a72c0d4e46d9be8fcf67d3e4be6cd85b27
lobrzut <48508079+lobrzut@users.noreply.github.com>
2021-07-22 13:21:41 +0200

    SendToDBoK

--- app/src/main/java/com/lobrzut/todok/ui/add/AddFragment.kt
+++ app/src/main/java/com/lobrzut/todok/ui/add/AddFragment.kt
@@ -1,60 +1,59 @@
 package com.lobrzut.todok.ui.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.lobrzut.todok.R
+import com.lobrzut.todok.database.TaskEntry
+import com.lobrzut.todok.databinding.FragmentAddBinding
+import com.lobrzut.todok.viewmodel.TaskViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val viewModel: TaskViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+
+
+        val binding = FragmentAddBinding.inflate(inflater)
+
+        val myAdapter = ArrayAdapter<String>(
+            requireActivity(),
+            android.R.layout.simple_spinner_dropdown_item,
+            resources.getStringArray(R.array.categories)
+        )
+
+        binding.apply {
+            spinner.adapter = myAdapter
+            btnAdd.setOnClickListener {
+                if (TextUtils.isEmpty((editTask.text))) {
+                    Toast.makeText(requireContext(), "It's Empty", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+
+                val title_str = editTask.text.toString()
+                val cat = spinner.selectedItemPosition
+
+                val taskEntry = TaskEntry(0,title_str, cat, System.currentTimeMillis())
+
+                viewModel.insert(taskEntry)
+                Toast.makeText(requireContext(), "Added Done!", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_addFragment_to_taskFragment)
+
+            }}
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
